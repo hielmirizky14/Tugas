c78d92548b68407d51b4d152d64f9b8205bd3771
hielmirizky14 <2413020@students.universitasmulia.ac.id>
2026-05-04 11:19:34 +0800

    Add files via upload

---
+++ app/learn/page.tsx
@@ -0,0 +1,12 @@
+export default function LearnPage() {
+    return(
+        <div>
+            <div>
+                <img src="/palette.svg" alt="Desain"></img>
+                <p>Desain</p>
+            </div>
+            <p>Figma</p>
+            <p>8 hours ago</p>
+        </div>
+    );
+}
